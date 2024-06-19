e436bc30efbd4be7235231f5872343d13e84e3a6
Herb Susmann <herbps10@gmail.com>
2024-06-19 15:54:47 -0400

    Added verbose option

--- R/estimators_direct.R
+++ R/estimators_direct.R
@@ -16,13 +16,15 @@
 #' Vector of learners to include in SuperLearner library for estimating treatment assignment mechanism.
 #' @param learners_outcome \[\code{character}\]\cr
 #' Vector of learners to include in SuperLearner library for estimating outcome regression.
+#' @param verbose \[\code{logical}]\cr
+#' Whether to print information messages during fitting
 #' @param control \[\code{standardization_control}\]\cr
 #' Additional tuning parameters for controlling fitting. Specify using \link{standardization_control}.
 #'
 #' @return A list of class \code{smr}
 #'
 #' @export
-direct_tmle <- function(data, trt, outcome, baseline, outcome_type = c("binomial", "continuous"), trt_method = "default", folds = 5, learners_trt = c("mean", "glm"), learners_outcome = c("mean", "glm"), control = standardization_control(), torch_params = list()) {
+direct_tmle <- function(data, trt, outcome, baseline, outcome_type = c("binomial", "continuous"), trt_method = "default", folds = 5, learners_trt = c("mean", "glm"), learners_outcome = c("mean", "glm"), verbose = FALSE, control = standardization_control(), torch_params = list()) {
   if(length(outcome_type) > 1) outcome_type <- outcome_type[1]
 
   task <- tsmr_Task$new(
@@ -36,8 +38,9 @@ direct_tmle <- function(data, trt, outcome, baseline, outcome_type = c("binomial
 
   g <- NULL
   riesz <- NULL
+  if(verbose == TRUE) cat("Starting treatment fitting\n")
   if(trt_method == "default") {
-    g <- treatment_probability(task, learners_trt, control$.return_full_fits, control$.learners_trt_folds)
+    g <- treatment_probability(task, learners_trt, control$.return_full_fits, control$.learners_trt_folds, verbose)
   }
   else if(tolower(trt_method) == "superriesz") {
     riesz <- riesz_representer(task, learners_trt, control$.return_full_fits, control$.learners_trt_folds, parameter = "direct", method = "superriesz")
@@ -46,6 +49,7 @@ direct_tmle <- function(data, trt, outcome, baseline, outcome_type = c("binomial
     riesz <- riesz_representer(task, learners_trt, control$.return_full_fits, control$.learners_trt_folds, parameter = "direct", method = "torch", torch_params = control$.torch_params)
   }
 
+  if(verbose == TRUE) cat("Starting outcome fitting\n")
   Qtilde <- outcome_regression(task, learners_outcome, include_treatment = TRUE, control$.return_full_fits, control$.learners_outcome_folds)
 
   fluctuations <- tmle_direct(task, Qtilde, g, riesz)
